ec23a8bb499a5fc7d37a8274fdbf8e9c5b81639c
Renan A. de M. Rocha <56835236+renanamr@users.noreply.github.com>
2022-02-13 16:43:30 -0300

    Adição do JML no MenuManager

--- src/main/java/menu/MenuManager.java
+++ src/main/java/menu/MenuManager.java
@@ -7,10 +7,14 @@ import java.io.IOException;
 public class MenuManager {
     //Pontos: 1
     //Renan
-    private static MenuManager instance;
+    private static /*@ spec_public @*/ MenuManager instance;
 
-    private MenuManager() {}
+    private /*@ pure @*/ MenuManager() {}
 
+    
+    /*@ assignable instance;
+    @ ensures \result == instance; 
+    @*/
     public static MenuManager getInstance() {
         if (instance == null) {
             instance = new MenuManager();
@@ -18,7 +22,7 @@ public class MenuManager {
         return instance;
     }
 
-    public void displaySelectedMenu(MenuItem menuItem) throws NotLoggedInException, NonExistentProductId, IOException {
+    public /*@ pure @*/ void displaySelectedMenu(/*@ non_null @*/MenuItem menuItem) throws NotLoggedInException, NonExistentProductId, IOException {
         menuItem.displayMenu();
     }
 }
